fedcc1ebeafc5734d9891e38590f6f45ed511183
karapeoples <kararpeoples@yahoo.com>
2020-06-23 21:58:38 -0500

    More stuff

--- routes/user/user_model.js
+++ routes/user/user_model.js
@@ -19,11 +19,11 @@ const erase = (id) => {
 return db('newUser').where('id', id).del()
 }
 
-const getUserFavs = (id,userId) => {
+const getUserFavs = (userId) => {
   return db.select('*')
     .from('saved as s')
     .join('newUser as n', 'n.id', 's.newUser_id')
-    .where('s.newUser_id', userId, 's.id', id)
+    .where('s.newUser_id', userId)
 }
 
 
